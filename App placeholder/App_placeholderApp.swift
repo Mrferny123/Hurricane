bdcd4d0eaae4486288495667b8ea0434f5065e7a
Nissi <nissiann@icloud.com>
2025-09-05 19:27:57 -0400

    Initial Commit

---
+++ App placeholder/App_placeholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_placeholderApp.swift
+//  App placeholder
+//
+//  Created by Nissi Sanju on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct App_placeholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App placeholderTests/App_placeholderTests.swift
@@ -0,0 +1,17 @@
+//
+//  App_placeholderTests.swift
+//  App placeholderTests
+//
+//  Created by Nissi Sanju on 9/5/25.
+//
+
+import Testing
+@testable import App_placeholder
+
+struct App_placeholderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
